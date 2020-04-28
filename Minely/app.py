19315a78280de1259a29619ea8852174a440366f
MattToegel <matthew.toegel@njit.edu>
2020-04-27 22:34:07 -0400

    test

--- Minely/app.py
+++ Minely/app.py
@@ -129,5 +129,7 @@ if __name__ == "__main__":
     app = create_app()
     setup_database(app)
     #app.run(debug=True)
-
+else:
+    app = create_app()
+    setup_database(app)
 
--- Minely/wsgi.py
+++ Minely/wsgi.py
@@ -1,5 +1,7 @@
-from app import create_app, setup_database
+
 #if __name__ == "__main__":
+from app import create_app, setup_database
+
 print('from wsgi')
 app = create_app()
 setup_database(app)
