509c0fec93dcb6e0e0a35e11a7275e432ad9928c
MattToegel <matthew.toegel@njit.edu>
2020-04-27 22:10:47 -0400

    wsgi

--- Minely/wsgi.py
+++ Minely/wsgi.py
@@ -1,5 +1,6 @@
-
 from app import create_app, setup_database
-app = create_app()
-setup_database(app)
-app.run()
+if __name__ == "__main__":
+    print('from wsgi')
+    app = create_app()
+    setup_database(app)
+    app.run()
