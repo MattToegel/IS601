f43a19ea9f3bb70ea52d1109cb3c1888668f081c
MattToegel <matthew.toegel@njit.edu>
2022-02-15 22:35:34 -0500

    Simple calc from class 02-15-22

---
+++ M4/MyCalc.py
@@ -0,0 +1,103 @@
+class MyCalc:
+    ans = 0
+
+    def _is_float(self, val):
+        try:
+            val = float(val)
+            return True
+        except:
+            return False
+
+    def _is_int(self, val):
+        try:
+            val = int(val)
+            return True
+        except:
+            return False
+
+    def _as_number(self, val):
+        if self._is_int(val):
+            return int(val)
+        elif self._is_float(val):
+            return float(val)
+        else:
+            raise Exception("Not a number")
+
+    def add(self, num1, num2):
+        if num1 == "ans":
+            return self.add(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1+num2
+        return self.ans
+
+    def sub(self, num1, num2):
+        if num1 == "ans":
+            return self.sub(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1-num2
+        return self.ans
+
+    def mult(self, num1, num2):
+        if num1 == "ans":
+            return self.mult(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            self.ans = num1*num2
+        return self.ans
+
+    def div(self, num1, num2):
+        if num1 == "ans":
+            return self.div(self.ans, num2)
+        else:
+            num1 = self._as_number(num1)
+            num2 = self._as_number(num2)
+            if num2 == 0:
+                print("Can't divide by zero, sorry")
+            else:
+                self.ans = num1/num2
+        return self.ans
+
+
+if __name__ == '__main__':
+    is_running = True
+    iSTR = input("Are you ready?")
+    calc = MyCalc()
+    print(calc)
+    if iSTR == "yes":
+        while is_running:
+            iSTR = input("What is your eq:")
+            if iSTR == "quit" or iSTR == "q":
+                is_running = False
+            else:
+                print("Your eq was " + str(iSTR))
+                if "+" in iSTR:
+                    nums = iSTR.split("+")
+                    r = calc.add(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done before - check to hanlde negative values
+                elif "/" in iSTR:
+                    nums = iSTR.split("/")
+                    r = calc.div(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+                elif "*" in iSTR or "x" in iSTR:
+                    nums = iSTR.split("*") if "*" in iSTR else iSTR.split("x")
+                    r = calc.mult(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+                # must be done last so negative numbers work
+                elif "-" in iSTR:
+                    nums = iSTR.split("-")
+                    r = calc.sub(nums[0].strip(), nums[1].strip())
+                    print("R is " + str(r))
+
+
+
+
+    else:
+        print("Good bye")
+        is_running = False
