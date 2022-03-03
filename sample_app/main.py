7ddd068676d84b44ca2ac3498df16dfc639af823
MattToegel <matthew.toegel@njit.edu>
2022-03-03 15:01:40 -0500

    in class test

---
+++ sample_app/main.py
@@ -0,0 +1,18 @@
+# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
+import os
+
+from flask import Flask
+
+
+# app = Flask(__name__)
+def create_app(config_filename=''):
+    app = Flask(__name__)
+    # app.config.from_pyfile(config_filename)
+    from sample_app.views.hello import hello
+    app.register_blueprint(hello)
+    return app
+
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
