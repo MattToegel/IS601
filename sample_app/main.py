095680fad927374e28af0ef568e5fcc837607526
MattToegel <matthew.toegel@njit.edu>
2022-03-03 19:50:33 -0500

    tweaking deploy

--- sample_app/main.py
+++ sample_app/main.py
@@ -8,7 +8,7 @@ from flask import Flask
 def create_app(config_filename=''):
     app = Flask(__name__)
     # app.config.from_pyfile(config_filename)
-    from sample_app.views.hello import hello
+    from views.hello import hello
     app.register_blueprint(hello)
     return app
 
