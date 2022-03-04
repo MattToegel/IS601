5080c8110532d356b80c47e940acb6579faa99c8
MattToegel <matthew.toegel@njit.edu>
2022-03-03 22:11:25 -0500

    cleaning up and used sys path instead

--- sample_app/main.py
+++ sample_app/main.py
@@ -1,8 +1,13 @@
 # from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
 import os
-
+import sys
 from flask import Flask
 
+# added so modules can be found between the two different lookup states:
+# from tests and from regular running of the app
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+print(CURR_DIR)
+sys.path.append(CURR_DIR)
 
 # app = Flask(__name__)
 
@@ -11,15 +16,11 @@ def create_app(config_filename=''):
     app = Flask(__name__)
     # app.config.from_pyfile(config_filename)
     with app.app_context():
-        try:
-            from views.hello import hello
-        except:
-            from sample_app.views.hello import hello
-
+        from views.hello import hello
         app.register_blueprint(hello)
         return app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
+
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
