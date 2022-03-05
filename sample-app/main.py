5f4de1ca0a7cce21cd6261ea1db7ec014db56a8a
MattToegel <matthew.toegel@njit.edu>
2022-03-04 19:36:38 -0500

    testing first app deploy

---
+++ sample-app/main.py
@@ -0,0 +1,16 @@
+# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
+import os
+
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    name = request.args.get('name', 'World')
+    return f'Hello {name}!'
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
