e21193937778a3080744a7811f0fa7cca4cbcba5
MattToegel <matthew.toegel@njit.edu>
2022-03-04 20:27:50 -0500

    testing pytest app factory test

--- sample_app/main.py
+++ sample_app/main.py
@@ -20,6 +20,6 @@ def create_app(config_filename=''):
         return app
 
 
-app = create_app()
 if __name__ == "__main__":
+    app = create_app()
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
