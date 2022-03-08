0ddbbefb13435da12b30c5f4c8ccb981cc101079
MattToegel <matthew.toegel@njit.edu>
2022-03-07 23:33:20 -0500

    baseline

--- sample_app/main.py
+++ sample_app/main.py
@@ -1,20 +1,27 @@
-# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
+
 import os
+import sys
+from flask import Flask
 
-from flask import Flask, request
+# added so modules can be found between the two different lookup states:
+# from tests and from regular running of the app
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+print(CURR_DIR)
+sys.path.append(CURR_DIR)
 
-
+# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
 # app = Flask(__name__)
 def create_app(config_filename=''):
     app = Flask(__name__)
     # app.config.from_pyfile(config_filename)
-    from sample_app.views.hello import hello
-    app.register_blueprint(hello)
-    return app
+    with app.app_context():
+        from views.hello import hello
+        app.register_blueprint(hello)
+        from views.mycalc import mycalc
+        app.register_blueprint(mycalc)
+        return app
 
 
 if __name__ == "__main__":
     app = create_app()
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
-
-
