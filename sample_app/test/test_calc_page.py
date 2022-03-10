c262b2ee1fef6503123dae8a5f20057e72293dd1
MattToegel <matthew.toegel@njit.edu>
2022-03-10 15:00:43 -0500

    updated calc

---
+++ sample_app/test/test_calc_page.py
@@ -0,0 +1,48 @@
+import pytest
+from bs4 import BeautifulSoup
+
+
+@pytest.fixture()
+def app():
+    from ..main import create_app
+    app = create_app()
+    """app.config.update({
+        "TESTING": True,
+    })"""
+
+    # other setup can go here
+
+    yield app
+
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    return app.test_cli_runner()
+
+
+def test_post_add_basic(client):
+    response = client.post("/mycalc/", data={"eq": "7+7"})
+    print(response.data)
+    assert b"value=\"14\"" in response.data
+
+
+def test_post_add(client):
+    response = client.post("/mycalc/", data={"eq": "7+7"})
+    html = response.data.decode("UTF-8")
+    # string to soup
+    soup = BeautifulSoup(html, 'html.parser')
+    # find the <input> tag with id "result"
+    result_ele = soup.find(id="result")
+    # get the value attribute of the input tag
+    assert result_ele.get("value") == "14"
+
+# https://www.twilio.com/blog/web-scraping-and-parsing-html-in-python-with-beautiful-soup
+# https://www.crummy.com/software/BeautifulSoup/bs4/doc/
+# https://stackabuse.com/convert-bytes-to-string-in-python/
--- sample_app/views/mycalc.py
+++ sample_app/views/mycalc.py
@@ -1,30 +1,32 @@
 from flask import Blueprint, request, render_template
 import calc.MyCalc
 
+c = calc.MyCalc.AdvMyCalc()  # here the state is persisted between requests (note it may be shared across users)
 mycalc = Blueprint('mycalc', __name__, url_prefix='/mycalc')
+
 
 @mycalc.route('/', methods=['GET'])
 def view_calc():
-    return render_template("my_calc.html", result="")
+    return render_template("my_calc.html")
+
 
 @mycalc.route('/', methods=['POST'])
 def do_calc():
-    c = calc.MyCalc.AdvMyCalc()
+    # c = calc.MyCalc.AdvMyCalc() # here the state resets each time
     checks = calc.MyCalc.AdvMyCalc.ops
-    handled = False
     data = request.form
     iSTR = data.get("eq")
     for check in checks:
-        if not handled and check in iSTR:
+        if check in iSTR:
             nums = iSTR.split(check)
+            if nums[0] == '':
+                nums[0] = "ans"
             try:
                 r = c.calc(nums[0].strip(), check, nums[1].strip())
 
                 print("R is " + str(r))
             except:
-                r = ""
-            handled = True
-            return render_template("my_calc.html", result=r)
-    if not handled:
-        print("The action you tried is not supported, please try again")
-    return render_template("my_calc.html", result="")
+                r = "ERROR"
+            return render_template("my_calc.html", result=r, eq=iSTR)
+    print("The action you tried is not supported, please try again")
+    return render_template("my_calc.html", result="UNSUPPORTED", eq=iSTR)
