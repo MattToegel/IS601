00405bda2c48e2a010500532d582e7f13cb03efa
MattToegel <matthew.toegel@njit.edu>
2022-03-21 19:19:41 -0400

    test env passing

--- sample_app/main.py
+++ sample_app/main.py
@@ -9,6 +9,7 @@ CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 print(CURR_DIR)
 sys.path.append(CURR_DIR)
 
+print("env var " + os.getenv("test"))
 
 # app = Flask(__name__)
 def create_app(config_filename=''):
