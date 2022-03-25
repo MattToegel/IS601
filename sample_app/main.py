7ffcd6e4d63856cd0feca7ba3426c51b0086bbe3
MattToegel <matthew.toegel@njit.edu>
2022-03-24 23:11:35 -0400

    Test heroku deploy change import path

--- sample_app/main.py
+++ sample_app/main.py
@@ -86,4 +86,4 @@ if __name__ == "__main__":
         # debugging output to verify tables were created
         for table in metadata.sorted_tables:
             print(table)
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8081)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
