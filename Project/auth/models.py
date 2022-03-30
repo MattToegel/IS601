422ae3b311a48ec85217971ab89efc7852769906
MattToegel <matthew.toegel@njit.edu>
2022-03-30 19:38:08 -0400

    baseline copied from sample_app

---
+++ Project/auth/models.py
@@ -0,0 +1,17 @@
+
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from base_model import db
+
+
+class User(UserMixin, db.Model):
+    username = db.Column(db.String(50), index=True, unique=True)
+    email = db.Column(db.String(150), unique=True, index=True)
+    password_hash = db.Column(db.String(150))
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
