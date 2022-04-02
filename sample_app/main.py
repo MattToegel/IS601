858e486e653a241cd10f0faa3cbdac471d9d00b9
MattToegel <matthew.toegel@njit.edu>
2022-04-01 23:05:36 -0400

    video walkthrough

--- sample_app/main.py
+++ sample_app/main.py
@@ -28,7 +28,8 @@ def create_app(config_filename=''):
         app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     # configure flask-login
-    SECRET_KEY = os.urandom(32)
+    SECRET_KEY = os.environ.get("SECRET_KEY", "thisisforlocal") # os.urandom(32 )
+
     app.config["SECRET_KEY"] = SECRET_KEY
 
     # app.config.from_pyfile(config_filename)
