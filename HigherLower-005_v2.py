5fe44e6c5de63d4b35cc39097d11a093ffb8bec0
MattToegel <matthew.toegel@njit.edu>
2022-09-27 14:33:31 -0400

    in-class conversion to Classes and methods

---
+++ HigherLower-005_v2.py
@@ -0,0 +1,57 @@
+import random
+
+
+class HigherLower:
+    def __init__(self) -> None:
+        self.choices = [*range(20)]
+        self.latest_number = random.choice(self.choices)
+        self.is_running = True
+        self.score = 0
+        self.score_increment = 1
+        self.score_max = 10
+
+    def start(self):
+        self.__run()
+
+    def check_input(self):
+        guess = input("Will the next number be higher or lower?").strip().lower()
+        if guess in ["higher","lower", "exit"]:
+            return guess
+        print("Please enter a valid option (higher, lower, exit)")
+        return self.check_input()
+        #self.check_input()
+
+    def __check_answer(self, guess):
+        next_number = random.choice(self.choices)
+        is_correct = (guess == "higher" and next_number > self.latest_number) \
+                or (guess == "lower" and next_number < self.latest_number)
+        self.latest_number = next_number
+        return is_correct
+    
+    def __check_score(self, is_correct):
+        if is_correct:
+            print("You're correct!")
+            self.score += self.score_increment
+            if self.score >= self.score_max:
+                print("You won it all!")
+                exit()
+        else:
+            print("Sorry that's not correct")
+    # the __ makes it "private" so not accessible outside of the class
+    def __run(self):
+        while self.is_running:
+            print(f"Your current score is {self.score}")
+            print(f"The current number is {self.latest_number}...")
+            guess = self.check_input()
+            print(f"You guessed {guess}")
+            if guess == "exit":
+                break
+            is_correct = self.__check_answer(guess)
+            self.__check_score(is_correct)
+
+
+
+
+hl = HigherLower()
+print(hl.__dict__)
+hl.start()
---
+++ HigherLower-007_v2.py
@@ -0,0 +1,59 @@
+import random
+
+
+class HigherLower:
+    def __init__(self) -> None:
+        
+        self.score = 0
+        self.is_running = True
+        self.score_max = 100
+        self.score_increment = 1
+        self.num_min = 1
+        self.num_max = 10
+        self.number = random.randint(self.num_min ,self.num_max)
+        self.guess = ""
+    def start(self):
+        # TODO change config stuff if we want
+        self.main()
+    # Asks for and validates a guess
+    def ask_guess(self):
+        self.guess = input("Is the next number higher or lower?").strip().lower()
+        if self.guess not in ["higher", "lower", "quit"]:
+            print("Please choose only higher or lower")
+            self.ask_guess()
+
+    def is_quitting(self):
+        if self.guess == "quit":
+            self.is_running = False
+            print("We're all done")
+            return True
+        return False
+    
+    def increment_score(self):
+        self.score += self.score_increment
+        print(f"You're right! Your score is {self.score}")
+        if self.score >= self.score_max:
+            print("Congrats you won!!! But at what cost?")
+            exit()
+
+    def check_win(self, next_number):
+        if (self.guess == "higher" and self.number < next_number) \
+            or (self.guess == "lower" and self.number > next_number):
+            self.increment_score()
+        else:
+            print("You're wrong :(")
+            
+        self.number = next_number
+
+    def main(self):
+        while self.is_running:
+            print(f"The current number is {self.number}")
+            self.ask_guess()
+            if not self.is_quitting():
+                next_number = random.randint(self.num_min ,self.num_max)
+                self.check_win(next_number)
+            
+
+hl = HigherLower()
+print(hl.__dict__)
+hl.start()
