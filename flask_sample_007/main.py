822919f0355d7ef6ccd903f631acce32f8c1fe92
MattToegel <matthew.toegel@njit.edu>
2022-10-17 17:13:50 -0400

    finding

--- flask_sample_007/main.py
+++ flask_sample_007/main.py
@@ -2,11 +2,12 @@ import os
 import sys
 from flask import Flask
 
+
 # added so modules can be found between the two different lookup states:
 # from tests and from regular running of the app
-"""CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
 print(CURR_DIR)
-sys.path.append(CURR_DIR)"""
+sys.path.append(CURR_DIR)
 
 
 # app = Flask(__name__)
@@ -14,7 +15,8 @@ def create_app(config_filename=''):
     app = Flask(__name__)
     # app.config.from_pyfile(config_filename)
     with app.app_context():
-        from views.hello import hello
+        
+        from views import hello
         app.register_blueprint(hello)
         return app
 
