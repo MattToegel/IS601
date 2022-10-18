de07bc9609d65fc0deee650ff3778dc006b79d11
MattToegel <matthew.toegel@njit.edu>
2022-10-18 14:01:18 -0400

    updated main/blueprints

--- flask_sample_005/main.py
+++ flask_sample_005/main.py
@@ -1,15 +1,25 @@
+# from https://towardsdatascience.com/deploy-to-google-cloud-run-using-github-actions-590ecf957af0
 import os
+import sys
+from flask import Flask
 
-from flask import Flask, request
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def hello_world():
-    name = request.args.get('name', 'World')
-    return f'Hello {name}!'
+# added so modules can be found between the two different lookup states:
+# from tests and from regular running of the app
+CURR_DIR = os.path.dirname(os.path.abspath(__file__))
+print(CURR_DIR)
+sys.path.append(CURR_DIR)
 
 
+# app = Flask(__name__)
+def create_app(config_filename=''):
+    app = Flask(__name__)
+    # app.config.from_pyfile(config_filename)
+    with app.app_context():
+        from views.hello import hello
+        app.register_blueprint(hello)
+        return app
+
+
+app = create_app()
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
