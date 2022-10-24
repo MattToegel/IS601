d00aa75306b0ae4ff25e30e857381eb50e290cbf
MattToegel <matthew.toegel@njit.edu>
2022-10-23 21:26:04 -0400

    tests

---
+++ flask_sample/sql/000_create_table_sample.sql
@@ -0,0 +1,8 @@
+CREATE TABLE
+    IS601_Sample(
+        id int auto_increment PRIMARY KEY,
+        name VARCHAR(20) unique,
+        val VARCHAR(40),
+        created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    )
