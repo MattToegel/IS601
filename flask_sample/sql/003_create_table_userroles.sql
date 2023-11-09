56f5ba3fe1f0c8863368e389afa36a8edb426039
MattToegel <Matthew.toegel@njit.edu>
2023-11-09 16:33:06 -0500

    Update 003_create_table_userroles.sql

--- flask_sample/sql/003_create_table_userroles.sql
+++ flask_sample/sql/003_create_table_userroles.sql
@@ -6,5 +6,7 @@ CREATE TABLE
         is_active tinyint(1) default 1,
         created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         modified TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-        UNIQUE KEY (role_id, user_id)
+        UNIQUE KEY (role_id, user_id),
+        FOREIGN KEY role_id REFERENCES IS601_Roles(id),
+        FOREIGN KEY user_id REFERENCES IS601_Users(id)
     )
