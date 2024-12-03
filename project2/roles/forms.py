4a8b043d65560a70f1c313e2b2a2869e305b51ad
Matt Toegel <matthew.toegel@njit.edu>
2024-12-03 15:20:28 -0500

    Delete project2 directory

--- project2/roles/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, BooleanField, SubmitField
-from wtforms.validators import DataRequired, Length, InputRequired
-
-class RoleForm(FlaskForm):
-    name = StringField("Role Name", validators=[DataRequired(),Length(1,20)])
-    description = TextAreaField ("Description", validators=[InputRequired()])
-    is_active = BooleanField("Is Active")
-    submit = SubmitField("Save")
--- project2/utils/lazy.py
+++
@@ -1,4 +0,0 @@
-class DictToObject:
-    def __init__(self, dictionary):
-        for key in dictionary:
-            setattr(self, key, dictionary[key])
